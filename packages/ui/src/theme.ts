00a38b3edbf1e6489b2b6465d1e19483ac6b0646
topdev1228 <topdeveloper1228@gmail.com>
2020-02-04 20:07:52 -0300

    feat(components): add button component

--- packages/ui/src/theme.ts
+++ packages/ui/src/theme.ts
@@ -1,11 +1,21 @@
 declare module 'styled-components' {
   export interface DefaultTheme {
     primary: string;
+    white: string;
+    green: string;
+    darkGradient: string;
+    lightGradient: string;
+    dark: string;
   }
 }
 
 const theme = {
-  primary: '#ffffff',
+  primary: '#333333',
+  white: '#ffffff',
+  green: '#008000',
+  darkGradient: 'linear-gradient(green, #333)',
+  lightGradient: 'linear-gradient(green, #f4f4f4)',
+  dark: '#333333',
 };
 
 export default theme;
