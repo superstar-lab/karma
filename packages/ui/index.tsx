4ceaa42a5804d477b26be6044e668d91c443152d
topdev1228 <topdeveloper1228@gmail.com>
2020-02-06 15:36:06 -0300

    feat(sign): add sign ui page

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,7 +1,7 @@
+import theme from './src/theme';
 import Button from './src/Button';
 import TextInput from './src/TextInput';
+import PhoneInput from './src/PhoneInput';
+import CodeInput from './src/CodeInput';
 
-export default {
-  Button,
-  TextInput,
-};
+export { theme, Button, TextInput, PhoneInput, CodeInput };
