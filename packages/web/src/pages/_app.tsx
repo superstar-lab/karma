ac6ea26a41ce9d71aaabb6caeee9717707e3f2ca
topdev1228 <topdeveloper1228@gmail.com>
2020-02-14 16:19:33 -0300

    fix(style): add styles to app file

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -4,10 +4,79 @@ import App from 'next/app';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import { theme } from '@karma/ui';
 
 import { store, persistor } from '../store';
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Avenir';
+    src: url('/fonts/AvenirBook.otf');
+    src: url('/fonts/AvenirMedium.otf');
+    src: url('/fonts/AvenirRoman.otf');
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  @media (max-width: 1080px) {
+    html {
+      font-size: 58%;
+    }
+  }
+
+  @media (max-width: 720px) {
+    html {
+      font-size: 54%;
+    }
+  }
+
+  @media (max-width: 440px) {
+    html {
+      font-size: 50%;
+    }
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-size: 14px;
+    font-family: 'Avenir', sans-serif;
+    font-weight: 500;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    border: 0;
+    cursor: pointer;
+  }
+`;
 
 export default class MyApp extends App {
   render() {
@@ -15,6 +84,7 @@ export default class MyApp extends App {
     return (
       <Provider store={store}>
         <PersistGate persistor={persistor}>
+          <GlobalStyle />
           <ThemeProvider theme={theme}>
             <Component {...pageProps} />
           </ThemeProvider>
--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -3,74 +3,6 @@ import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet, createGlobalStyle } from 'styled-components';
 
 //fonts don't load correctly without createGlobalStyle
-const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'Avenir';
-    src: url('/fonts/AvenirBook.otf');
-    src: url('/fonts/AvenirMedium.otf');
-    src: url('/fonts/AvenirRoman.otf');
-  }
-
-  * {
-    margin: 0;
-    padding: 0;
-    outline: 0;
-    box-sizing: border-box;
-  }
-
-  *:focus {
-    outline: 0;
-  }
-
-  html, body, #root {
-    height: 100%;
-  }
-
-  html {
-    font-size: 62.5%;
-  }
-
-  @media (max-width: 1080px) {
-    html {
-      font-size: 58%;
-    }
-  }
-
-  @media (max-width: 720px) {
-    html {
-      font-size: 54%;
-    }
-  }
-
-  @media (max-width: 440px) {
-    html {
-      font-size: 50%;
-    }
-  }
-
-  body {
-    -webkit-font-smoothing: antialiased;
-  }
-
-  body, input, textarea, button {
-    font-size: 14px;
-    font-family: 'Avenir', sans-serif;
-    font-weight: 500;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  ul {
-    list-style: none;
-  }
-
-  button {
-    border: 0;
-    cursor: pointer;
-  }
-`;
 
 export default class MyDocument extends Document {
   static async getInitialProps({ renderPage }) {
@@ -92,7 +24,6 @@ export default class MyDocument extends Document {
           <title>Karma</title>
           <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700&display=swap" rel="stylesheet" />
           {this.props.styleTags}
-          <GlobalStyle />
         </Head>
         <body>
           <Main />
